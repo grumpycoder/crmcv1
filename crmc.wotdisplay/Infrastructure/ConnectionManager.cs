c8a213d64c1a343079d26dae37378f3b086d4379
Mark Lawrence <grumpycoder@outlook.com>
2016-01-22 15:16:26 -0600

    update wot connectionmanager class

--- crmc.wotdisplay/Infrastructure/ConnectionManager.cs
+++ crmc.wotdisplay/Infrastructure/ConnectionManager.cs
@@ -2,60 +2,48 @@
 using Microsoft.AspNet.SignalR.Client;
 using System.Diagnostics;
 using System.Net.Http;
+using NLog;
 
 namespace crmc.wotdisplay.Infrastructure
 {
     public class ConnectionManager
     {
-        private static readonly string ServerURI = Settings.Default.WebServerUrl + "/signalr";
+        private static readonly string ServerUri = Settings.Default.WebServerUrl + "/signalr";
         private static readonly string HubName = Settings.Default.HubName;
-        private static readonly HubConnection connection = new HubConnection(ServerURI);
+        private static readonly HubConnection connection = new HubConnection(ServerUri);
         public static IHubProxy HubProxy;
 
-        //private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         public static string KioskName = Settings.Default.KioskName;
 
         public static HubConnection Connection { get { return connection; } }
 
-        //public static async void ConnectAsync()
-        public static void ConnectAsync()
+        public static async void ConnectAsync()
         {
             Connection.StateChanged += ConnectionOnStateChanged;
 
             HubProxy = Connection.CreateHubProxy(HubName);
             try
             {
-                //await Connection.Start();
                 Connection.Start().ContinueWith(task =>
                 {
                     if (task.IsFaulted) return;
                     if (Connection.State != ConnectionState.Connected) return;
 
-                    //Logger.Trace("{0} Connected", KioskName);
+                    Logger.Info(string.Format("{0} Connected", KioskName));
                     Debug.WriteLine("{0} Connected", KioskName);
-                    HubProxy.Invoke("Join", KioskName).ContinueWith(taskJoin =>
-                    {
-                        if (taskJoin.IsFaulted)
-                        {
-                            //Logger.Error("{0} Error during joining the server", KioskName);
-                            Debug.WriteLine("{0} Error during joining the server", KioskName);
-                        }
-                        else
-                        {
-                            var sub = HubProxy.Subscribe("addMessage");
-                        }
-                    });
+
                 }).Wait();
 
             }
             catch (HttpRequestException e)
             {
-                //Logger.Error(e.InnerException);
+                Logger.Error(e.InnerException);
                 Debug.WriteLine(e.InnerException);
             }
             catch (HttpClientException clientException)
             {
-                //Logger.Error(clientException.InnerException);
+                Logger.Error(clientException.InnerException);
                 Debug.WriteLine(clientException.InnerException);
             }
         }
@@ -63,23 +51,6 @@ namespace crmc.wotdisplay.Infrastructure
         private static void ConnectionOnStateChanged(StateChange stateChange)
         {
             Debug.WriteLine("Connection State: " + Connection.State);
-        }
-
-        public static void SendMessage(string message)
-        {
-            HubProxy.Invoke("SendMessage", message).ContinueWith(task =>
-            {
-                if (task.IsFaulted)
-                {
-                    Debug.WriteLine("{0} Error during sending message", KioskName);
-                    //Logger.Trace("{0} Error during sending message", KioskName);
-                }
-                else
-                {
-                    //Logger.Trace("{0} Message send successfully", KioskName);
-                    Debug.WriteLine("{0} Message send successfully", KioskName);
-                }
-            });
         }
 
     }
